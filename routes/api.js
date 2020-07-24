623ce9a206bba55aa9475d7b538289d1c4f7f544
sunnyRK <radadiyasunny970@gmail.com.com>
2020-07-24 14:09:13 +0530

    Added new alpi for chainlink oracles

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models/models');
 const walletModels = require('../models/walletmodels');
+const chainlinkmodel = require('../models/chainlinkmodel');
+
+router.post('/createLinkTrade', function(req, res, next) {
+    models.create(req.body).then(function(trade) {
+        res.send(trade)
+    }).catch(next);
+});
+
+router.get('/getAllLinkTrades', async (req, res) => {
+    try {
+        var result = await models.find().exec();
+        res.send(result);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 router.post('/createTrade', function(req, res, next) {
     models.create(req.body).then(function(trade) {
